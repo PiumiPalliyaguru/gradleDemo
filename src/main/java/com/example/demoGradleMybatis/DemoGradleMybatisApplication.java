f79a8eb2ef8cdf3aa351211deedf87f6db19438c
Piumi Palliyaguru <52409218+ppalliyaguru@users.noreply.github.com>
2020-07-05 11:52:40 +0530

    initiate

---
+++ src/main/java/com/example/demoGradleMybatis/DemoGradleMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoGradleMybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradleMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoGradleMybatis/DemoGradleMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoGradleMybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
